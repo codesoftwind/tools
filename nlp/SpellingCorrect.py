2b040ee83fb40ad5cbe54d754f9e57a1b5d96553
Ryan Wang <wangdong@RyandeMacBook-Pro.local>
2017-09-12 12:46:58 +0800

    添加了获取word的同义词的功能

--- nlp/SpellingCorrect.py
+++ nlp/SpellingCorrect.py
@@ -1,10 +1,23 @@
+#-*-coding:utf-8-*-
 from textblob import *
 
 def sentenceSpellCorrect(sentence):
+    '''输入：字符串
+       输出：字符串'''
     return TextBlob(sentence).correct()
 
 def wordSpellCorrect(word):
+    '''输入：字符串
+       输出：字符串'''
     return Word(word).spellcheck()[0][0]
+
+def getSynonyms(word):
+    '''输入：字符串
+       输出：字符串的list'''
+    synonyms = []
+    for synset in Word(word).synsets:
+        synonyms += synset.lemma_names()
+    return synonyms
 
 if __name__ == '__main__':
     sentence = "i havv goog speling"
